545532b6ad9fd0f8eb128de6a4af44d7b9d4371d
Kongdp <kongdepeng@xiaoniangao.com>
2021-04-20 16:07:13 +0800

    deploy

---
+++ precache-manifest.4f8ec250c1f89d6f4994e201b364c9c7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f43589bec41a61a8c3ef2b46b01930c",
+    "url": "/s-ui/index.html"
+  },
+  {
+    "revision": "f6b2d029881589d6a5ae",
+    "url": "/s-ui/static/css/Navigation.e2a5d681.chunk.css"
+  },
+  {
+    "revision": "94cf099a7855cd90230f",
+    "url": "/s-ui/static/css/main.913b951c.chunk.css"
+  },
+  {
+    "revision": "6184bd3a8a601b307429",
+    "url": "/s-ui/static/js/4.59e4e9f6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/s-ui/static/js/4.59e4e9f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f99a936d1d558aae201",
+    "url": "/s-ui/static/js/Home.0dbc9b79.chunk.js"
+  },
+  {
+    "revision": "f6b2d029881589d6a5ae",
+    "url": "/s-ui/static/js/Navigation.7622b154.chunk.js"
+  },
+  {
+    "revision": "94cf099a7855cd90230f",
+    "url": "/s-ui/static/js/main.034c817a.chunk.js"
+  },
+  {
+    "revision": "15113abce770eefb93f6",
+    "url": "/s-ui/static/js/runtime-main.ccbddce3.js"
+  },
+  {
+    "revision": "6f577b005088ff53bc96aaa5512dd3b6",
+    "url": "/s-ui/static/media/emoji.6f577b00.png"
+  }
+]);
---
+++ static/js/Home.0dbc9b79.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonps-ui"]=this["webpackJsonps-ui"]||[]).push([[0],{42:function(e,n,t){"use strict";t.r(n);var s=t(0),u=t.n(s);n.default=function(){return u.a.createElement("div",null,"home")}}}]);
